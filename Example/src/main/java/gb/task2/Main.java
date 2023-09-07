73b53a27c53c40eb26cf5f53397a9d7cd2c56a67
kvo52@kvo1 <kvo.52@mail.ru>
2023-09-07 22:31:44 +0300

    new folder and docs

---
+++ Example/src/main/java/gb/task2/Main.java
@@ -0,0 +1,29 @@
+package gb.task2;
+
+import gb.task1.Decorator;
+import gb.task1.OtherClass;
+
+/**
+ * Основной класс приложения. Здесь мы можем описать
+ * его основное назначение и способы взаимодействия с ним.
+ *
+ */
+public class Main {
+
+    /**
+     * Точка входа в программу. С неё всегда всё начинается.
+     *
+     * @param args стандартные аргументы командной строки
+     */
+    public static void main (String[] args) {
+        int result = OtherClass.sum(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.sub(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.mul(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = OtherClass.div(2, 2);
+        System.out.println(Decorator.decorate(result));
+    }
+
+}
