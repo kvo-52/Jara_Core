42e3af69221d3ac25bd5a76d81e992b9488ff8c9
kvo52@kvo1 <kvo.52@mail.ru>
2023-09-14 13:29:15 +0300

    HW02_main

---
+++ Exp02/src/main/java/ru/gb/jcore/Main.java
@@ -0,0 +1,15 @@
+package ru.gb.jcore;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int iTmp = 0;
+        Scanner sc = new Scanner(System.in);
+
+
+        // Инициализация поля (Field initialization)
+
+
+    }
+}
