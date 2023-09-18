91827cab1bcaed4768058670cccf6d1432d27331
kvo-52 <94926993+kvo-52@users.noreply.github.com>
2023-09-18 22:18:39 +0300

    Update Worker.java

--- HW03_01/src/main/java/ru/gb/jcore/Worker.java
+++ HW03_01/src/main/java/ru/gb/jcore/Worker.java
@@ -10,7 +10,9 @@ public class Worker {
     /** Абстрактный метод расчёта среднемесячной заработной платы
      *
      */
-    public abstract double calculateAverageSalary();
+    public abstract double calculateAverageSalary(){
+        return x;
+    };
 
     public String getName() {
         return name;
