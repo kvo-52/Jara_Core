c3215cac9c000b54af3f1270d521f8e94071c1d0
kvo52@kvo1 <kvo.52@mail.ru>
2023-09-18 22:21:05 +0300

    HW_03 abstract

--- HW03_01/src/main/java/ru/gb/jcore/Worker.java
+++ HW03_01/src/main/java/ru/gb/jcore/Worker.java
@@ -7,10 +7,12 @@ public class Worker {
         this.name = name;
     }
 
-    /** Абстрактный метод расчёта среднемесячной заработной платы
-     *
+    /**
+     * Абстрактный метод расчёта среднемесячной заработной платы
      */
-    public abstract double calculateAverageSalary();
+    public double calculateAverageSalary() {
+        return 0;
+    }
 
     public String getName() {
         return name;
